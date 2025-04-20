cd34bd07c95ce084f8dcd4489c92afed67df19c5
Denisa-Iulia Vaidasigan <denisa-iulia.vaidasigan@stud.th-deg.de>
2025-04-20 11:52:04 +0200

    Learning TailWind and Working on Components

---
+++ src/components/PredictionButton.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const PredictionButton: React.FC = () => {
+  return (
+    <button className="bg-blue-500 hover:bg-blue-400 text-white font-bold py-2 px-4 border-b-4 border-blue-700 hover:border-blue-500 rounded">
+        Predict
+    </button>
+  );
+};
+
+export default PredictionButton;
---
+++ src/pages/SurvivalCalculatorUI.tsx
@@ -0,0 +1,9 @@
+import PredictionButton from "../components/PredictionButton";
+
+function SurvivalCalculatorUI() {
+    return (
+        <div>PredictionButton</div>
+    )
+}
+
+export default SurvivalCalculatorUI;    
