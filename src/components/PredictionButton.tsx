c74381955006687ba8f35c9ece7464a2a0c0552f
Denisa-Iulia Vaidasigan <denisa-iulia.vaidasigan@stud.th-deg.de>
2025-04-20 14:22:12 +0200

    Beginning Survival Calculator Page Creation

--- src/components/PredictionButton.tsx
+++ src/components/PredictionButton.tsx
@@ -1,9 +1,16 @@
 import React from 'react';
 
 const PredictionButton: React.FC = () => {
+  const handleClick = () => {
+    console.log("Clicked!");
+  };
+
   return (
-    <button className="bg-blue-500 hover:bg-blue-400 text-white font-bold py-2 px-4 border-b-4 border-blue-700 hover:border-blue-500 rounded">
-        Predict
+    <button 
+      onClick={handleClick}
+      className="bg-background hover:bg-blue-400 text-foreground font-bold py-2 px-4 border-b-4 border-blue-700 hover:border-blue-500 rounded"
+    >
+      Predict
     </button>
   );
 };
--- src/pages/SurvivalCalculatorUI.tsx
+++ src/pages/SurvivalCalculatorUI.tsx
@@ -2,7 +2,20 @@ import PredictionButton from "../components/PredictionButton";
 
 function SurvivalCalculatorUI() {
     return (
-        <div>PredictionButton</div>
+        <div className="min-h-screen bg-background text-foreground">
+            <header className="p-4 shadow-md">
+                <h1 className="text-4xl font-bold">Survivor Prediction Calculator</h1>
+                <p className="mt-2 text-lg">Predicts the likelihood of survival during the Titanic disaster based on key factors from your input.</p>
+            </header>
+
+            <main>
+            
+            </main>
+
+            <footer>
+                <PredictionButton />
+            </footer>
+        </div>
     )
 }
 
