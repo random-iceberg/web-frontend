55c8d9ebc2f01153e2b5430ab6c7e95ca1ac5293
FaresM7 <fareselbermawy@gmail.com>
2025-05-29 11:51:58 +0200

    Configured babel and other components.

--- jest.config.js
+++ jest.config.js
@@ -4,7 +4,14 @@ module.exports = {
     moduleNameMapper: {
       '\\.(css|less|scss|sass)$': 'identity-obj-proxy',
       '^@/(.*)$': '<rootDir>/src/$1',
+      '^services/(.*)$': '<rootDir>/src/services/$1'
     },
     setupFilesAfterEnv: ['<rootDir>/src/setupTests.ts'],
-  };
-  
+    transform: {
+      '^.+\\.(ts|tsx)$': 'ts-jest',
+      '^.+\\.(js|jsx)$': 'babel-jest'
+    },
+    transformIgnorePatterns: [
+      "/node_modules/(?!(axios)/)"
+    ]
+};
