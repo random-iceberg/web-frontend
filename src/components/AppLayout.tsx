9618141f120c10017c93eb4a15f1e6e72a4e3904
HlexNC <hlexhelftd@gmail.com>
2025-06-16 00:30:54 +0200

    fix: correct import path for ConnectionStatus component

--- src/components/AppLayout.tsx
+++ src/components/AppLayout.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import { Outlet } from "react-router-dom";
 import Navbar from "components/Navbar";
 import Footer from "components/Footer";
-import ConnectionStatus from "components/connectionStatus";
+import ConnectionStatus from "components/ConnectionStatus";
 
 /*
   This Component is our App Layout, adding common structure to all pages.
