072f3902fc44fcff5b5723e8d78ffbac807ca21f
HlexNC <hlexhelftd@gmail.com>
2025-07-01 23:45:04 +0200

    fix(AppLayout): add Footer component to AppLayout and remove from Layout

--- src/components/AppLayout.tsx
+++ src/components/AppLayout.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { Outlet } from "react-router-dom";
 import Navbar from "components/Navbar";
+import Footer from "components/Footer";
 import ConnectionStatus from "components/ConnectionStatus";
 
 /*
@@ -17,6 +18,7 @@ const Layout: React.FC = () => {
           <Outlet />
         </div>
       </main>
+      <Footer />
       <ConnectionStatus />
     </div>
   );
